fee0a70e2b837d340ae86d874067dcaf7b4c3fb6
myNameIsJeff <123896394+FreDo-JS@users.noreply.github.com>
2026-04-23 16:24:26 +0200

    Mega update!!! zobacz sam!

--- App.jsx
+++ App.jsx
@@ -2,6 +2,7 @@ import { use, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
+import 'bootstrap/dist/css/bootstrap.css'
 import "./App.css";
 import { Route, Routes, Link, BrowserRouter } from "react-router";
 function JobOffer({ title, company, type, stack, desc }) {
@@ -26,9 +27,78 @@ function JobOffer({ title, company, type, stack, desc }) {
     </>
   );
 }
-function JobBoard() {
+function JobBoard({jobs}) {
   const [filter, setFilter] = useState("all");
-  const jobs = [
+ 
+  const filteredJobs =
+    filter === "all" ? jobs : jobs.filter((job) => job.type === filter);
+
+  return (
+    <>
+      <section>
+        <button
+          onClick={() => setFilter("Remote")}
+          style={{ backgroundColor: filter === "Remote" ? "yellow" : "white" }}
+        >
+          Remote
+        </button>
+        <button onClick={() => setFilter("On-site")}>On-site</button>
+        <button onClick={() => setFilter("all")}>Wszystkie</button>
+      </section>
+      <main>
+        {filteredJobs.map((job) => (
+          <JobOffer key={job.id} {...job} />
+        ))}
+      </main>
+    </>
+  );
+}
+function AddJob({jobs, setJobs})
+{
+  const [form, setForm] = useState({
+    company: "", title: "", type: '', stack: '', desc: ''
+  })
+
+  function handleChange(e)
+  {
+     setForm({...form, [e.target.name]: e.target.value})
+  }
+
+  function handleSubmit()
+  {
+    if(!form.company || !form.title)
+    {
+      alert('Wprowadz dane')
+      return
+    }
+    const newOffer ={
+      ...form,
+      id: jobs.length + 1,
+      stack: form.stack.split(',').map(s => s.trim())
+
+    }
+    setJobs([...jobs, newOffer])
+    alert('Dodano!')
+  }
+  return <>
+  <h1>Dodaj oferte</h1>
+  <div className="form-group">
+      <input className="form-control" type="text" name="company" placeholder="Nazwa firmy" onChange={handleChange}/>
+      <input className="form-control" type="text" name="title" placeholder="Stanowisko" onChange={handleChange}/>
+      {/* <p>Opcje pracy</p> */}
+      <select className="form-control" name="type" id="" value={form.type} onChange={handleChange}>
+        <option value="">Wybierz</option>
+        <option value="On-site">On-site</option>
+        <option value='Remote'>Remote</option>
+      </select>
+      <input className="form-control" type="text" name="stack" placeholder="Technologie" onChange={handleChange}/>
+      <textarea className="form-control" name="desc" cols={30} rows={10} id="" onChange={handleChange}></textarea>
+      <button onClick={handleSubmit} className="btn btn-primary">Dodaj</button>
+  </div>
+  </>
+}
+function App() {
+  const [jobs, setJobs] = useState( [
     {
       id: 1,
       company: "SGB-Bank",
@@ -53,31 +123,7 @@ function JobBoard() {
       stack: ["React", "JavaScript", "Bycie sigma"],
       desc: "Dla nauczyciela",
     },
-  ];
-  const filteredJobs =
-    filter === "all" ? jobs : jobs.filter((job) => job.type === filter);
-
-  return (
-    <>
-      <section>
-        <button
-          onClick={() => setFilter("Remote")}
-          style={{ backgroundColor: filter === "Remote" ? "yellow" : "white" }}
-        >
-          Remote
-        </button>
-        <button onClick={() => setFilter("On-site")}>On-site</button>
-        <button onClick={() => setFilter("all")}>Wszystkie</button>
-      </section>
-      <main>
-        {filteredJobs.map((job) => (
-          <JobOffer key={job.id} {...job} />
-        ))}
-      </main>
-    </>
-  );
-}
-function App() {
+  ])
   return (
     <>
       <BrowserRouter>
@@ -88,8 +134,8 @@ function App() {
           <Link to="/contact">Kontakt</Link>
         </nav>
         <Routes>
-          <Route path="/" element={<JobBoard />} />
-          <Route path="/add" element={<h1>Dodaj</h1>} />
+          <Route path="/" element={<JobBoard  jobs={jobs}/>} />
+          <Route path="/add" element={<AddJob jobs={jobs} setJobs={setJobs}/>} />
           <Route path="/contact" element={<h1>Kontakt</h1>} />
         </Routes>
       </BrowserRouter>
